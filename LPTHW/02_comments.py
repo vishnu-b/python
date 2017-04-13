ca98b784410694990c0df8098d7320efd569694b
Vishnu B <vishnu.nxg@gmail.com>
2017-04-13 11:22:50 +0530

    LPTHW - ex2 done

---
+++ LPTHW/02_comments.py
@@ -0,0 +1,9 @@
+# Comments are important. Try to write self documenting code, readable code.
+# Comments are for others who read your code.
+# Or for you after one month.
+
+print "Comment can be found anywhere. But don't overdo it." # this
+
+# Comments help in debugging. Comment out the code you don't want to be run.
+# Print "This won't run".
+print "This won't run."
