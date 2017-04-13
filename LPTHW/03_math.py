527849ce1a1dff21ecd350c58767225d18f837a0
Vishnu B <vishnu.nxg@gmail.com>
2017-04-13 11:37:59 +0530

    LPTHW - ex3 done

---
+++ LPTHW/03_math.py
@@ -0,0 +1,27 @@
+# I have chickens.
+print "I'll now  count my chickens."
+
+print "Hens", 25 + 30 / 5
+print "Roosters", 100 - 25 * 3 % 4
+
+# Hens and Roosters together make eggs. Let's count them.
+# PEDMAS
+print "Let's count the eggs."
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Check if 3 + 2 < 5 -7 or not
+print "Is it true that 3 + 2 < 5 - 7?"
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "OH! False."
+
+# Let's check if 3 + 2 >  5- 7
+print "How about some more. Huh?"
+
+print "Is it greater?", 3 + 2 > 5 - 7
+print "Is it greater than or equal?", 3 + 2 >= 5 - 7
+print "Is it less than or equal?", 3 + 2 <= 5 - 7
+
