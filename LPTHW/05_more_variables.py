c935f959120babb724ef64f691931f96cf38675c
Vishnu B <vishnu@sourceeasy.com>
2017-07-17 12:07:14 +0530

    LPTHW - ex 05 done.

---
+++ LPTHW/05_more_variables.py
@@ -0,0 +1,17 @@
+my_name = "Vishnu B"
+my_age = 27
+my_height = 167 #cms
+my_weight = 74 #kgs
+my_eyes = "Brown"
+my_teeth = "White"
+my_hair = "Black"
+
+print "Let's talk about %s." % my_name
+print "He is %d cms tall." % my_height
+print "He is %d kgs heavy." % my_weight
+print "Actually he is almost into the overweight category."
+print "He has got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are %s." % my_teeth
+
+print "If I add %d, %d and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+
